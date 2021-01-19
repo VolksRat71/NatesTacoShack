4853edaf18644cb68d4def022c24334bb90b17c6
VolksRat71 <nathanieljryan1994@gmail.com>
2021-01-19 17:03:33 -0600

    adding siteMetadata to gatsby-config.js

--- gatsby/src/components/Footer.js
+++ gatsby/src/components/Footer.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 export default function Footer() {
     return (
-        <div>
+        <>
             <footer>
                 <p>&copy; Nates Taco Shack {new Date().getFullYear()}</p>
             </footer>
-        </div>
+        </>
     )
 };

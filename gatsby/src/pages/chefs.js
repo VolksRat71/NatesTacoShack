5f14fc1d22e9c46531326727a1e0be2a249ac62d
VolksRat71 <nathanieljryan1994@gmail.com>
2021-01-25 22:08:40 -0600

    adding alll of the chefs to the chefs page and styled :)

--- gatsby/src/pages/chefs.js
+++ gatsby/src/pages/chefs.js
@@ -1,9 +1,80 @@
 import React from 'react';
+import { graphql, Link } from 'gatsby';
+import Img from 'gatsby-image';
+import styled from 'styled-components';
 
-export default function TacoMastersPage() {
+const ChefsGrid = styled.div`
+    display: grid;
+    grid-gap: 2rem;
+    grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+`;
+
+const SingleChef = styled.div`
+    .gatsby-image-wrapper {
+        height: 400px;
+    }
+    h2 {
+        transform: rotate(-2deg);
+        text-align: center;
+        font-size: 3rem;
+        margin-bottom: -2rem;
+        position: relative;
+        z-index: 2;
+    }
+    .description {
+        border: 0.1rem solid var(--purple);
+        background: var(--grey);
+        padding: 1rem;
+        margin: 2rem;
+        margin-top: -6rem;
+        z-index: 2;
+        position: relative;
+        transform: rotate(1deg);
+        text-align: center;
+    }
+`;
+
+export default function ChefPage({ data: { allSanityPerson: { chefs } } }) {
+    console.log(chefs);
     return (
         <>
-            <p>TacoMastersPage</p>
+            <ChefsGrid>
+                {chefs.map(({
+                    id, name, description,
+                    slug: { current },
+                    image: { asset: { fluid } }
+                }) => (
+                    <SingleChef key={id}>
+                        <Link to={`chefs/${current}`}>
+                            <h2><span className="mark">{name}</span></h2>
+                            <Img fluid={fluid} />
+                        </Link>
+                        <p className="description">{description}</p>
+                    </SingleChef>
+                ))}
+            </ChefsGrid>
         </>
     )
 };
+
+export const query = graphql`
+    query {
+        allSanityPerson {
+            chefs: nodes {
+                name
+                id
+                description
+                slug {
+                    current
+                }
+                image {
+                    asset {
+                        fluid(maxWidth: 400){
+                            ...GatsbySanityImageFluid
+                        }
+                    }
+                }
+            }
+        }
+    }
+`;

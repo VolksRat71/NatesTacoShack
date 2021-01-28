696a19c926fd5bcdacf193071eae0032c1a50143
VolksRat71 <nathanieljryan1994@gmail.com>
2021-01-27 22:39:31 -0600

    Orders page displaying form with custom oject to state hook

--- gatsby/src/pages/order.js
+++ gatsby/src/pages/order.js
@@ -1,11 +1,44 @@
 import React from 'react';
 import SEO from '../components/SEO';
+import useForm from '../utils/useForm';
 
 export default function OrdersPage() {
+    const { values, updateValue } = useForm({
+        name: '',
+        email: ''
+    })
+
+    const { name, email } = values;
+    console.log(values);
+
     return (
         <>
             <SEO title="Delivery &amp; Curbside" />
-            <p>OrdersPage</p>
+            <form>
+                <fieldset>
+                    <legend>Your Info</legend>
+                    <label htmlFor="name">Name</label>
+                    <input
+                        type="text"
+                        name="name"
+                        value={name}
+                        onChange={updateValue}
+                    />
+                    <label htmlFor="email">Email</label>
+                    <input
+                        type="email"
+                        name="email"
+                        value={email}
+                        onChange={updateValue}
+                    />
+                </fieldset>
+                <fieldset>
+                    <legend>Menu</legend>
+                </fieldset>
+                <fieldset>
+                    <legend>Order</legend>
+                </fieldset>
+            </form>
         </>
     )
 };

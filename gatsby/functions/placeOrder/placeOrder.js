8e79f99a33f6d91427ba4aefa5758b2d349c101f
VolksRat71 <nathanieljryan1994@gmail.com>
2021-01-31 16:34:55 -0600

    nodemailer serverless function is functional!

--- gatsby/functions/placeOrder/placeOrder.js
+++ gatsby/functions/placeOrder/placeOrder.js
@@ -1,12 +1,24 @@
 const nodemailer = require('nodemailer');
 
-const transporter = nodemailer.createItem({
-    host: process.env.MAIL_HOST,
-    port: process.env.MAIL_PORT,
-    secure: false,
-    requireTLS: true,
+const transporter = nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
     auth: {
-        user: process.env.MAIL_USER,
-        pass: process.env.MAIL_PASS
+        user: 'zora90@ethereal.email',
+        pass: 'f6EQbB8dWaxtdX7EAR'
     }
-})
+});
+
+exports.handler = async (event, context) => {
+    const info = await transporter.sendMail({
+        from: "Nates Taco Shack <nate@example.com>",
+        to: "Orders@email.com",
+        subject: "New order!",
+        html: `<p>Taco Order!</p>`
+    })
+    console.log(info);
+    return {
+        statusCode: 200,
+        body: JSON.stringify(info)
+    }
+}

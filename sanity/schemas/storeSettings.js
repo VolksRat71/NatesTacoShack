7cc314043c354bfe402c602e85bf6eb94df063fc
VolksRat71 <nathanieljryan1994@gmail.com>
2021-02-03 15:34:59 -0600

    adding new logo to repo

---
+++ sanity/schemas/storeSettings.js
@@ -0,0 +1,14 @@
+import { GiHomeGarage as icon } from 'react-icons/gi';
+
+export default {
+    name: 'storeSettings',
+    title: 'Settings',
+    type: 'document',
+    icon,
+    fields: [
+        {
+            name: 'chef',
+            title: 'Who is on today?'
+        }
+    ]
+}

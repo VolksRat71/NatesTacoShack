dccbc64eff0359d37fe40ae750478b248d77953c
VolksRat71 <nathanieljryan1994@gmail.com>
2021-02-03 18:11:38 -0600

    new sanity schema for setting up chefs and tacos for the homepage

--- sanity/schemas/schema.js
+++ sanity/schemas/schema.js
@@ -6,11 +6,12 @@ import schemaTypes from 'all:part:@sanity/base/schema-type';
 import taco from './taco';
 import ingredient from './ingredient';
 import person from './person';
+import storeSettings from './storeSettings';
 
 export default createSchema({
   // We name our schema
   name: 'default',
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
-  types: schemaTypes.concat([taco, ingredient, person]),
+  types: schemaTypes.concat([taco, ingredient, person, storeSettings]),
 });
--- sanity/schemas/storeSettings.js
+++ sanity/schemas/storeSettings.js
@@ -1,16 +1,28 @@
-import { GiHomeGarage as icon } from 'react-icons/gi';
+import { FaStoreAlt as icon } from 'react-icons/fa';
 
 export default {
     name: 'storeSettings',
-    title: 'Settings',
+    title: 'Home Page Settings',
     type: 'document',
     icon,
     fields: [
+        {
+            name: 'name',
+            title: 'Store ID',
+            type: 'string',
+            description: 'Please describe the store Name or Number'
+        },
         {
             name: 'chef',
             title: 'Who is on today?',
             type: 'array',
             of: [{ type: 'reference', to: [{ type: 'person' }] }]
-        }
+        },
+        {
+            name: 'specials',
+            title: 'Daily Specials',
+            type: 'array',
+            of: [{ type: 'reference', to: [{ type: 'taco' }] }]
+        },
     ]
 }
